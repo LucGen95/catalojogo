fda66eb4ddc22ade945eec199fac7289b6e90c63
LucGen <genuino.lucas09@gmail.com>
2025-10-10 17:43:16 -0300

    Commit adding jenkins to the project

--- backend/src/main/java/com/rgv/catalojogo/game/entity/Game.java
+++ backend/src/main/java/com/rgv/catalojogo/game/entity/Game.java
@@ -1,12 +1,15 @@
-package com.rgv.catalojogo.game.entity;
-
-import jakarta.persistence.Entity;
-import lombok.Data;
-
-@Entity
-@Data
-public class Game {
-
-
-
-}
+//package com.rgv.catalojogo.game.entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//import lombok.Data;
+//
+//@Entity
+//@Data
+//public class Game {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//}
